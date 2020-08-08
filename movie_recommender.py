3719eaa8eef0709de09a7a084b55306a9734e77a
SharmaAman26 <69337973+SharmaAman26@users.noreply.github.com>
2020-08-08 20:16:05 +0530

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -8,25 +8,25 @@ import requests
 def get_movies_from_tastedive(movie):
     parameters= {'q':movie,'type':'movies','limit':5}
     parameters['k']='381469-Movierec-K01LBLDL'
-    page=requests.get('https://tastedive.com/api/similar',params=parameters)
-    return page.json()
+    respage=requests.get('https://tastedive.com/api/similar',params=parameters)
+    return respage.json()
 
-def extract_movie_titles(movie_dict):
+def get_movie_titles(movie_dict):
     titles=[movie['Name'] for movie in movie_dict["Similar"]['Results'] ]
     return titles
 
-def get_related_titles(movie_list):
-    title_list=[title for movie in movie_list for title in extract_movie_titles(get_movies_from_tastedive(movie)) ]
+def get_titles_list(movie_list):
+    title_list=[title for movie in movie_list for title in get_movie_titles(get_movies_from_tastedive(movie)) ]
     return list(set(title_list))
 
 
-def get_movie_data(movie_name):
+def extract_movie_info(movie_name):
     parameters={'t':movie_name,'r':'json'}
     parameters['apikey']='fee51582'
     movie_info=requests.get('http://www.omdbapi.com/',params=parameters)
     return movie_info.json()
 
-def get_movie_rating(movie_info):
+def get_rating(movie_info):
     for rating_info in movie_info['Ratings']:
         if rating_info['Source']=='Rotten Tomatoes':
             rating=int(rating_info['Value'][ :2])
@@ -35,23 +35,23 @@ def get_movie_rating(movie_info):
             rating=0
     return rating
 
-def get_sorted_recommendations():
-        prompt='Enter name of movie and seperate the movies using , '
-        userinput=str(input('Enter name of movie and seperate the movies using ,'))
+def give_sorted_recommendations():
+        prompt='Enter name of movie and seperate the movies using comma '
+        userinput=str(input(prompt))
         movie_list=userinput.strip().split(',')
 
         try:
-             related_titles=get_related_titles(movie_list)
+             related_titles=get_titles_list(movie_list)
 
-             recommended_list=sorted(related_titles,key=lambda title: (get_movie_rating(get_movie_data(title)),title),reverse=True)
+             recommended_list=sorted(related_titles,key=lambda title: (get_rating(extract_movie_info(title)),title),reverse=True)
              if len(recommended_list)>1:
                  print(recommended_list)
                  return recommended_list
              else:
                  print ('Cannot recognise that movie.Try with different movie')
         except Exception:
-             errmsg=' type the name properly.Please restart the program  , '
+             errmsg=' Type the name properly and Please restart the program '
              print(err_msg)
 
 
-get_sorted_recommendations()
+give_sorted_recommendations()
